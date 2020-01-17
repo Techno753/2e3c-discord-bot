ce51a8386374d0b94ef2c1a557cc5ab656e8ddb7
Jung-Heon Lee <Techno753@gmail.com>
2020-01-18 03:13:49 +1300

    Added command to shutdown the bot.

--- src/main/java/events/StartEvent.java
+++ src/main/java/events/StartEvent.java
@@ -1,9 +1,15 @@
 package events;
 
+import net.dv8tion.jda.api.EmbedBuilder;
 import net.dv8tion.jda.api.events.ReadyEvent;
+import net.dv8tion.jda.api.events.message.guild.GuildMessageReceivedEvent;
 import net.dv8tion.jda.api.hooks.ListenerAdapter;
 import tools.ConfigTool;
 import tools.TagTool;
+
+import java.util.regex.Pattern;
+
+import static tools.VerifyMsgTool.*;
 
 /**
  * Code to execute upon the bot readying
@@ -16,5 +22,30 @@ public class StartEvent extends ListenerAdapter {
         ConfigTool.printConfig(re.getJDA());
         TagTool.readTags();
         System.out.println("Ready!");
+    }
+
+    public void onGuildMessageReceived(GuildMessageReceivedEvent gmre) {
+        // Get message as raw String
+        String msgIn = gmre.getMessage().getContentRaw();
+        String cmdString = "";
+        String msgOut = "";
+        boolean msgSet = false;
+        String msgType = "";
+        EmbedBuilder eb = new EmbedBuilder();
+
+        if (msgIn.length() > 1 && hasCorrectPrefix(gmre)) {
+            cmdString = msgIn.toLowerCase().substring(1);
+        }
+
+        // Adds a text tag
+        if (Pattern.matches("^(?is)shutdown$", cmdString) &&
+                isBotCreator(gmre)) {
+            {
+                // shutdown bot
+                System.out.println("Shutting down bot");
+                gmre.getJDA().shutdown();
+                System.exit(0);
+            }
+        }
     }
 }
